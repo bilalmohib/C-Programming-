78743859f254b0edb5e73cdb69b4ba56cf279a27
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2021-07-17 16:07:28 +0500

    Done practise upto page number 35

---
+++ Learn-C-the-hardway-practise/program3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+//The main starting of the program
+int main(int argc,char *argv[])
+{
+    int distance = 100;
+
+    //printing the statement
+    printf("You are %d miles away.\n",distance);
+    return 0;
+}
